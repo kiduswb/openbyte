8edcca221276ba54c0814edf806502e02ba82917
Kidus <mail@kidus.ca>
2024-10-15 16:20:24 -0400

    fix: site password compare

--- public/dashboard/site.php
+++ public/dashboard/site.php
@@ -78,7 +78,7 @@
             exit;
         }
 
-        if($site->cpanel_password == $_POST['password']) {
+        if(strcmp($_POST['password'], $site->cpanel_password)) {
             echo 
             '<div class="alert rounded-0 bg-warning-custom text-white">
                 <i class="fas fa-exclamation-triangle me-2"></i>
