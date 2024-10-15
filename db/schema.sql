d5c94c9bb156fc21de504a90d8bc9f02bdc6de19
Kidus <mail@kidus.ca>
2024-10-14 20:04:11 -0400

    Update: database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,82 @@
-CREATE TABLE users (
-    id TEXT PRIMARY KEY,
-    email TEXT NOT NULL,
-    pwd_hash TEXT NOT NULL,
-    is_verified SMALLINT NOT NULL,
-    timestamp INT NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE sites (
-    id TEXT PRIMARY KEY,
-    userid TEXT,
-    label TEXT,
-    subdomain TEXT,
-    internal_id TEXT,
-    cpanel_username TEXT,
-    cpanel_password TEXT,
-    timestamp INT
-);
 
-CREATE TABLE reset_tokens (
-    userid VARCHAR(255) PRIMARY KEY,
-    token VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `openbyte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_tokens`
+--
+
+CREATE TABLE `reset_tokens` (
+  `userid` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sites`
+--
+
+CREATE TABLE `sites` (
+  `id` varchar(255) NOT NULL,
+  `userid` varchar(255) DEFAULT NULL,
+  `label` text DEFAULT NULL,
+  `subdomain` text DEFAULT NULL,
+  `internal_id` text DEFAULT NULL,
+  `cpanel_username` text NOT NULL,
+  `cpanel_password` text DEFAULT NULL,
+  `timestamp` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(255) NOT NULL,
+  `email` text NOT NULL,
+  `pwd_hash` text NOT NULL,
+  `is_verified` smallint(6) NOT NULL,
+  `timestamp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reset_tokens`
+--
+ALTER TABLE `reset_tokens`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `sites`
+--
+ALTER TABLE `sites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
