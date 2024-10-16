e4437518569de38f975c8dafda4001a4d254f55f
Kidus <mail@kidus.ca>
2024-10-15 20:10:06 -0400

    added: Google analytics tag

--- public/dashboard/header.php
+++ public/dashboard/header.php
@@ -3,10 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- SEO Tags -->
+    <title><?php echo $pageTitle; ?></title>
+    
+    <!-- Dependencies -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <title><?php echo $pageTitle; ?></title>
     <script src="https://unpkg.com/htmx.org@2.0.3" integrity="sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq" crossorigin="anonymous"></script>
+
+    <!-- Google Analytics -->
+    <?php 
+        if($_ENV['TRACK_GOOGLE_ANALYTICS'] == "true") 
+        {
+            $gtag_id = $_ENV['G_TAG_ID'];
+
+            echo <<<_GTAG
+            <script async src="https://www.googletagmanager.com/gtag/js?id=$gtag_id"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '$gtag_id');
+            </script>
+_GTAG;
+        }
+    ?>
 </head>
